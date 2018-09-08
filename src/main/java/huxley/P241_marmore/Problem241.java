e4faadb25ba8b2fa9973b34017be10c7b8f2fc28
Danihcv <dhcv@ic.ufal.br>
2018-09-07 23:46:36 -0300

    Implementação e teste padrão do problema 241

---
+++ src/main/java/huxley/P241_marmore/Problem241.java
@@ -0,0 +1,48 @@
+package huxley.P241_marmore;
+
+import java.util.Scanner;
+
+public class Problem241 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int i, j, n, q, teste = 0, busca;
+        boolean found;
+        n = scan.nextInt();
+        q = scan.nextInt();
+        while (n != 0 && q != 0) {
+            int[] num = new int[n + 1];
+            for (i = 1; i <= n; i++) {
+                num[i] = scan.nextInt();
+            }
+            for (j = n; j > 1; j--) {
+                for (i = 1; i < j; i++) {
+                    if (num[i] > num[i + 1]) {
+                        num[0] = num[i];
+                        num[i] = num[i + 1];
+                        num[i + 1] = num[0];
+                    }
+                }
+            }
+            teste++;
+            System.out.printf("CASE# %d:" + System.lineSeparator(), teste);
+            for (; q > 0; q--) {
+                found = false;
+                busca = scan.nextInt();
+                for (i = 1; i <= n; i++) {
+                    if (num[i] == busca) {
+                        System.out.printf("%d found at %d" + System.lineSeparator(), busca, i);
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    System.out.printf("%d not found" + System.lineSeparator(), busca);
+                }
+            }
+
+            n = scan.nextInt();
+            q = scan.nextInt();
+        }
+    }
+}
--- src/test/java/huxley/Problem241Test.java
+++ src/test/java/huxley/Problem241Test.java
@@ -1,6 +1,6 @@
 package huxley;
 
-import huxley.P546_arvore_busca_binaria.Problem546;
+import huxley.P241_marmore.Problem241;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -9,16 +9,16 @@ import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class Problem546Test extends GenericTest {
+class Problem241Test extends GenericTest {
     @Test
     void main() throws IOException {
-        setUpInputAndExpectedListsFromResources("P546_arvore_busca_binaria/");
+        setUpInputAndExpectedListsFromResources("P241_marmore/");
 
         if (inputList != null && expectedList != null) {
             for (int i = 0; i < inputList.length; i++) {
                 setInputStream(new String(Files.readAllBytes(Paths.get(inputList[i].getPath()))));
                 String expected = new String(Files.readAllBytes(Paths.get(expectedList[i].getPath())));
-                Problem546.main(new String[]{});
+                Problem241.main(new String[]{});
                 assertEquals(expected, outContent.toString());
                 outContent.reset();
             }
