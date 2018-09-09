9b23ae9277bf9701b5c73efe29f408c60ef4017d
Danihcv <dhcv@ic.ufal.br>
2018-09-09 19:33:34 -0300

    Melhoria na classe GenericTest

--- src/test/java/huxley/Problem179Test.java
+++ src/test/java/huxley/Problem179Test.java
@@ -4,8 +4,6 @@ import huxley.P179_batalha_naval.Problem179;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,8 +14,8 @@ class Problem179Test extends GenericTest {
 
         if (inputList != null && expectedList != null) {
             for (int i = 0; i < inputList.length; i++) {
-                setInputStream(new String(Files.readAllBytes(Paths.get(inputList[i].getPath()))));
-                String expected = new String(Files.readAllBytes(Paths.get(expectedList[i].getPath())));
+                setInputStream(inputList[i]);
+                String expected = fileContentToString(expectedList[i]);
                 Problem179.main(new String[]{});
                 assertEquals(expected, outContent.toString());
                 outContent.reset();
